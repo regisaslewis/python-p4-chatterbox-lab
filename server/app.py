ac222a6eea96791e33544c7c6d4f06cf5ad84337
RegisLewis <regisaslewis@gmail.com>
2023-10-26 20:11:00 -0700

    chatterbox lab

--- server/app.py
+++ server/app.py
@@ -14,13 +14,57 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods=["GET", "POST"])
 def messages():
-    return ''
+    if request.method == "GET":
+        messages_list = []
+        messages = Message.query.all()
+        for n in messages:
+            messages_list.append(n.to_dict())
 
-@app.route('/messages/<int:id>')
+        return make_response(messages_list, 200)
+    
+    elif request.method == "POST":
+        post_content = request.get_json()
+        new_message = Message(
+            body=post_content["body"],
+            username=post_content["username"]
+        )
+
+        db.session.add(new_message)
+        db.session.commit()
+
+        return make_response(new_message.to_dict(), 201)
+
+@app.route('/messages/<int:id>', methods=["GET", "PATCH", "DELETE"])
 def messages_by_id(id):
-    return ''
+    message = Message.query.filter_by(id = id).first()
+
+    if message == None:
+        response_body = {
+            "message": f"Message #{id} can't be found."
+        }
+        return make_response(response_body, 404)
+    elif request.method == "GET":
+        return make_response(message.to_dict(), 200)
+    elif request.method == "PATCH":
+        patch_content = request.get_json()
+        for attr in patch_content:
+            setattr(message, attr, patch_content[attr])
+        db.session.add(message)
+        db.session.commit()
+
+        return make_response(message.to_dict(), 200)
+        
+    elif request.method == "DELETE":
+        db.session.delete(message)
+        db.session.commit()
+
+        response_body = {
+            "deleted": True,
+            "message": f"Message #{id} has been deleted."
+        }
+        return make_response(response_body, 200)
 
 if __name__ == '__main__':
     app.run(port=5555)
